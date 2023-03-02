aefa631c982e6616ab6e754c413fdf3cc1e54514
Edward <Edward Bryan Abergas>
2023-03-02 10:07:00 +0800

    Added settings modal

---
+++ app/src/main/java/io/bry1337/pomfocus/android/ui/home/SettingsModal.kt
@@ -0,0 +1,24 @@
+package io.bry1337.pomfocus.android.ui.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.bry1337.pomfocus.android.ui.theme.AppTheme
+
+/**
+ * Created by Bryan on 3/2/23.
+ * Copyright (c) 2023 bry1337.github.io. All rights reserved.
+ */
+
+@Composable
+fun SettingsModal(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsModalPreview() {
+    AppTheme {
+        SettingsModal()
+    }
+}
