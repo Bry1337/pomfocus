372e0d149d2c137ef60b5f4776566b3890feb26b
Edward <Edward Bryan Abergas>
2023-03-02 18:28:25 +0800

    Implemented settings screen - Added settings modal - Added bottom sheet operation for bottom sheet handling (further refactor on the code needs to be done) - Added shape extensions - Fixed issue with deprecated methods

--- app/src/main/java/io/bry1337/pomfocus/android/ui/home/HomeScreen.kt
+++ app/src/main/java/io/bry1337/pomfocus/android/ui/home/HomeScreen.kt
@@ -2,42 +2,78 @@ package io.bry1337.pomfocus.android.ui.home
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.bry1337.pomfocus.android.R
+import io.bry1337.pomfocus.android.extensions.RoundedModalShape
 import io.bry1337.pomfocus.android.ui.components.AppNavBar
 import io.bry1337.pomfocus.android.ui.components.AppNavBarItem
 import io.bry1337.pomfocus.android.ui.theme.AppTheme
+import io.bry1337.pomfocus.android.utils.BottomSheetDescriptor
+import io.bry1337.pomfocus.android.utils.rememberBottomSheetOperation
 
 /**
  * Created by Bryan on 3/1/23.
  * Copyright (c) 2023 bry1337.github.io. All rights reserved.
  */
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterialApi::class)
+private enum class HomeScreenBottomSheet : BottomSheetDescriptor {
+    Settings;
+
+    override val launchPresentation: ModalBottomSheetValue
+        get() = when (this) {
+            Settings -> ModalBottomSheetValue.Expanded
+            else -> ModalBottomSheetValue.HalfExpanded
+        }
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Scaffold(
-        modifier = modifier.fillMaxSize(),
-        topBar = {
-            TopAppbar()
-        }
+    val scope = rememberCoroutineScope()
+    val bottomSheetOperation = rememberBottomSheetOperation(
+        scope = scope,
+        initialBottomSheetDescriptor = HomeScreenBottomSheet.Settings
+    )
+    val onSettingsPressed = {
+        bottomSheetOperation.presentSheet(HomeScreenBottomSheet.Settings)
+    }
+    ModalBottomSheetLayout(
+        sheetState = bottomSheetOperation.sheetState,
+        sheetContent = {
+            when (bottomSheetOperation.sheetDescriptor) {
+                HomeScreenBottomSheet.Settings -> SettingsModal()
+            }
+        },
+        sheetShape = RoundedModalShape
     ) {
+        Scaffold(
+            modifier = modifier.fillMaxSize(),
+            topBar = {
+                TopAppbar(onSettingsPressed)
+            }
+        ) {
+        }
     }
 }
 
 @Composable
-private fun TopAppbar() {
+private fun TopAppbar(onSettingsPressed: () -> Unit) {
     AppNavBar(
         leadingItem = {
             AppNavBarItem(icon = R.drawable.ic_menu)
         },
         trailingItem = {
-            AppNavBarItem(icon = R.drawable.ic_settings)
+            AppNavBarItem(icon = R.drawable.ic_settings, action = onSettingsPressed)
         }
     )
 }
--- app/src/main/java/io/bry1337/pomfocus/android/ui/home/SettingsModal.kt
+++ app/src/main/java/io/bry1337/pomfocus/android/ui/home/SettingsModal.kt
@@ -1,9 +1,17 @@
 package io.bry1337.pomfocus.android.ui.home
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.bry1337.pomfocus.android.ui.theme.AppTheme
+import io.bry1337.pomfocus.theme.DimensionConstants
 
 /**
  * Created by Bryan on 3/2/23.
@@ -12,7 +20,20 @@ import io.bry1337.pomfocus.android.ui.theme.AppTheme
 
 @Composable
 fun SettingsModal(modifier: Modifier = Modifier) {
-
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Scaffold {
+            Column(
+                modifier = modifier.fillMaxSize().padding(it),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(DimensionConstants.hSpacing.md.dp)
+            ) {
+                Text("Hello World")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
