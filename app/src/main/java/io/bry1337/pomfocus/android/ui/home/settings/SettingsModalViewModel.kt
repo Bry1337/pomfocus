7776dcd93966fa7ba2e4147792acb38e0d30a654
Edward <Edward Bryan Abergas>
2023-03-15 13:42:14 +0800

    Added theme selection and dark mode in settings modal - Added resources - Fixed dropdown menu height - Updated home screen detail colors

--- app/src/main/java/io/bry1337/pomfocus/android/ui/home/settings/SettingsModalViewModel.kt
+++ app/src/main/java/io/bry1337/pomfocus/android/ui/home/settings/SettingsModalViewModel.kt
@@ -1,9 +1,13 @@
 package io.bry1337.pomfocus.android.ui.home.settings
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.bry1337.pomfocus.android.ui.theme.ThemeManager
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -12,4 +16,34 @@ import javax.inject.Inject
  */
 
 @HiltViewModel
-class SettingsModalViewModel @Inject constructor() : ViewModel()
+class SettingsModalViewModel @Inject constructor() : ViewModel() {
+    val themePresetNameList = ThemeManager.themePresetNames
+    var theme by mutableStateOf(ThemeManager.theme)
+        private set
+
+    private val isDarkScheme = theme.isDarkScheme
+
+    var isDarkMode by mutableStateOf(isDarkScheme)
+        private set
+
+    init {
+        viewModelScope.launch {
+            ThemeManager.themeFlow.collect {
+                theme = it
+            }
+        }
+    }
+
+    fun onPresetNameChanged(presetName: String) {
+        viewModelScope.launch {
+            ThemeManager.onThemePresetNameChanged(presetName)
+        }
+    }
+
+    fun onDarkModeSwitch(isChecked: Boolean) {
+        viewModelScope.launch {
+            ThemeManager.onDarkSchemeChanged(isChecked)
+        }
+        isDarkMode = isChecked
+    }
+}
