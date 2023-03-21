77a85dcdf64509be17e1f8b91cf516c068b47cd7
Edward <Edward Bryan Abergas>
2023-03-21 09:33:48 +0800

    Implemented app launcher icon - Added more resources - Updated splash screen - Updated documents

--- app/src/main/java/io/bry1337/pomfocus/android/ui/notification/NotificationProvider.kt
+++ app/src/main/java/io/bry1337/pomfocus/android/ui/notification/NotificationProvider.kt
@@ -46,7 +46,7 @@ object NotificationProvider {
     ) {
         createNotificationChannel(descriptionText = channelDescription)
         val notificationBuilder = NotificationCompat.Builder(appContext, NOTIFICATION_CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_menu)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle(appContext.getString(contentTitle))
             .setContentText(appContext.getString(contentText))
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
