cee029a452218dd365b28bcba218e9e191c62cde
Edward <Edward Bryan Abergas>
2023-03-24 14:20:59 +0800

    Implemented save of tasks to local database - Updated viewmodel saving logic - Updated logic of saving model to db model - Updated builder

--- shared/src/commonMain/kotlin/io/bry1337/pomfocus/db/TaskOpsImpl.kt
+++ shared/src/commonMain/kotlin/io/bry1337/pomfocus/db/TaskOpsImpl.kt
@@ -1,16 +1,16 @@
 package io.bry1337.pomfocus.db
 
 import com.squareup.sqldelight.runtime.coroutines.asFlow
-import com.squareup.sqldelight.runtime.coroutines.mapToList
-import com.squareup.sqldelight.runtime.coroutines.mapToOne
+import io.bry1337.pomfocus.domain.models.Task
 import io.bry1337.pomfocus.shared.db.Database
-import io.bry1337.pomfocus.shared.db.Task
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
@@ -24,12 +24,52 @@ class TaskOpsImpl(private val dispatcher: CoroutineDispatcher = Dispatchers.Defa
     private val db = Database.shared
     private val scope = CoroutineScope(SupervisorJob() + dispatcher)
 
+    /**
+     * Returns a list of Tasks from the Db.
+     *
+     * The process is queried from the Db then the model is mapped from the db is then
+     * mapped to a local model for application view and usage
+     * then return it as flow
+     */
     override fun listObjects(pageSize: Long): Flow<List<Task>> {
-        return db.pomfocusQueries.listAllTasks().asFlow().mapToList(dispatcher)
+        val taskList: ArrayList<Task> = ArrayList()
+        return db.pomfocusQueries.listAllTasks().asFlow().map {
+            it.executeAsList()
+        }.map {
+            it.asFlow().map { dbTask ->
+                taskList.toMutableList().also {
+                        list ->
+                    list.add(Task.build(dbTask.id, dbTask.description, dbTask.datetime!!))
+                }
+            }
+        }.flatMapLatest {
+            it
+        }
     }
 
     override fun getObject(id: String): Flow<Task> {
-        return db.pomfocusQueries.getTask(id = id).asFlow().mapToOne(dispatcher)
+        return db.pomfocusQueries.getTask(id = id).asFlow().map {
+            it.executeAsOne()
+        }.flatMapLatest { dbTask ->
+            flowOf(Task.build(dbTask.id, dbTask.description, dbTask.datetime!!))
+        }
+    }
+
+    override suspend fun saveObjects(tasks: List<Task>) {
+        withContext(scope.coroutineContext) {
+            tasks.asFlow().map {
+                it
+            }.collect { task ->
+                if (task.id != "0") {
+                    val dbTask = Task.buildDbTask(task.id, task.description, task.dateTime)
+                    db.pomfocusQueries.insertTask(
+                        id = dbTask.id,
+                        description = dbTask.description,
+                        datetime = dbTask.datetime
+                    )
+                }
+            }
+        }
     }
 
     override suspend fun saveObject(task: Task) {
@@ -37,20 +77,8 @@ class TaskOpsImpl(private val dispatcher: CoroutineDispatcher = Dispatchers.Defa
             db.pomfocusQueries.insertTask(
                 id = task.id,
                 description = task.description,
-                datetime = task.datetime
+                datetime = task.dateTime
             )
-        }
-    }
-
-    override suspend fun saveObjects(tasks: List<Task>) {
-        withContext(scope.coroutineContext) {
-            tasks.asFlow().map {
-                db.pomfocusQueries.insertTask(
-                    id = it.id,
-                    description = it.description,
-                    datetime = it.datetime
-                )
-            }
         }
     }
 
