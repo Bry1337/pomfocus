d371e21275a61d77c0f82e02491c4002693270de
Edward <Edward Bryan Abergas>
2024-06-13 15:57:20 +0800

    Modified timer

--- app/src/main/java/io/bry1337/pomfocus/android/utils/TimerConstants.kt
+++ app/src/main/java/io/bry1337/pomfocus/android/utils/TimerConstants.kt
@@ -12,9 +12,9 @@ object TimerConstants {
     //    const val DEFAULT_POMODORO_TOTAL_SECONDS = 60 * 25 // 60 = 1 minute * 25 minutes
     const val DEFAULT_BREAK_TOTAL_SECONDS = 60 * 5
 
-    const val DEFAULT_POMODORO_MINUTES = 1 // Temporary value
+//    const val DEFAULT_POMODORO_MINUTES = 1 // Temporary value
 
-//    const val DEFAULT_POMODORO_MINUTES = 25 // Default minutes per Pomodoro technique
+    const val DEFAULT_POMODORO_MINUTES = 25 // Default minutes per Pomodoro technique
     const val DEFAULT_BREAK_MINUTES = 5
 
     const val DEFAULT_SECONDS = 0
